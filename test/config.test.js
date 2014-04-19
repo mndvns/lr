ff3e84bd6de5f04482029dd52711ab90f5d471f3
Michael Vanasse <mail@mndvns.com>
2014-04-18 23:30:57 -0600

    add config reader

---
+++ test/config.test.js
@@ -0,0 +1,18 @@
+
+var should = require('should');
+var lrrc = require('../lib/lrrc');
+
+describe('lrrc.read', function(){
+  it('should read strings', function(){
+    lrrc.read('{"buzz": "bazz"}').should.eql({buzz: {cmd:'bazz', ignore: false}});
+    lrrc.read('{"herp": "@derp"}').should.eql({herp: {cmd:'derp', ignore: true}});
+    lrrc.read('{buzz: bazz}').should.throw();
+  });
+
+  it('should read yaml strings', function(){
+    lrrc.read('foo: echo bar').should.eql({foo: {cmd:'echo bar', ignore: false}});
+    lrrc.read('{"aaa": "@bbb"}').should.eql({aaa: {cmd:'bbb', ignore: true}});
+    lrrc.read('foo:echo bar').should.throw();
+  });
+});
+
--- test/lr.test.js
+++ test/lr.test.js
@@ -2,6 +2,9 @@
  * Module dependencies
  */
 
+var exec = require('child_process').exec;
+var should = require('should');
+
 describe('lr', function() {
 
 });
